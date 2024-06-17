68ebb8af28a5c0bbd7b9a1822e4373bb00ada26c
lynacyna <cenguluna@gmail.com>
2024-06-17 12:24:09 +0200

    hhhhhhh

--- Assets/MyGame/Scripts/Manager.cs
+++ Assets/MyGame/Scripts/Manager.cs
@@ -1,13 +1,15 @@
-
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
-    public TMP_Text randomNumberText, hilfsText, erklaerungsText, counterText;
-    private int currentRandomNumber;
-    private int correctCount = 0;
+    public TMP_Text randomNumberText;
+    public TMP_Text hilfsText;
+    public TMP_Text erklaerungsText;
+    public TMP_Text counterText;
+    public TMP_Text winText;
+
     public AudioClip correct;
     public AudioClip wrong;
 
@@ -15,145 +17,223 @@ public class Manager : MonoBehaviour
     public Image buzz;
     public Image fizzbuzz;
 
-    public Camera maincam;
-    public Color backColor;
+    public Button startAgainButton;
+    public Button newNumberButton;
 
+    public Camera mainCamera;
+    public Color backgroundColor;
+
+    public RawImage correctImage; // Neues RawImage für richtige Antworten
+    public RawImage wrongImage;   // Neues RawImage für falsche Antworten
+
+    private int currentRandomNumber;
+    private int correctCount = 0;
 
     void Start()
     {
-        GenerateRandomNumber();
-        UpdateCounterText();
-    }
-
-
-    void GenerateRandomNumber()
-    {
-        currentRandomNumber = Random.Range(1, 50);
-        randomNumberText.text = currentRandomNumber.ToString();
-        maincam.backgroundColor = backColor;
-    }
-
-    void UpdateCounterText()
-    {
-        counterText.text = "Counter: " + correctCount.ToString() + "/10";
+        InitializeGame();
     }
 
     void Update()
     {
-        if (correctCount < 10)
-        {
-            if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.Alpha1))
-            {
-                CheckFizz(currentRandomNumber);
-                fizz.color = Color.gray;
-                Invoke("ResetColor", 2f);
-                
-
-            }
-            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.Alpha2))
-            {
-                CheckBuzz(currentRandomNumber);
-                buzz.color = Color.gray;
-                Invoke("ResetColor", 2f);
-
-            }
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Alpha3))
-            {
-                CheckFizzBuzz(currentRandomNumber);
-                fizzbuzz.color = Color.grey;
-                Invoke("ResetColor", 2f); 
-            }
-
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                GenerateRandomNumber();
-                hilfsText.text = "";
-                erklaerungsText.text = ""; 
-            }
-        }
-
+        HandleUserInput();
     }
 
+    private void InitializeGame()
+    {
+        correctCount = 0;
+        startAgainButton.gameObject.SetActive(false);
+        winText.gameObject.SetActive(false);
+        correctImage.gameObject.SetActive(false); // Bilder am Anfang ausblenden
+        wrongImage.gameObject.SetActive(false);   // Bilder am Anfang ausblenden
+        EnableGameElements(true);
+        GenerateRandomNumber();
+        UpdateCounterText();
+    }
 
-    void PlaySound(AudioClip clip)
+    private void GenerateRandomNumber()
+    {
+        currentRandomNumber = Random.Range(1, 100);
+        randomNumberText.text = currentRandomNumber.ToString();
+        mainCamera.backgroundColor = backgroundColor;
+    }
+
+    private void UpdateCounterText()
+    {
+        counterText.text = "Counter: " + correctCount.ToString() + "/10";
+        if (correctCount >= 10)
+        {
+            GameWon();
+        }
+    }
+
+    private void HandleUserInput()
+    {
+        if (correctCount >= 10) return;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            CheckFizz(currentRandomNumber);
+            SetTemporaryColor(fizz, Color.gray);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            CheckBuzz(currentRandomNumber);
+            SetTemporaryColor(buzz, Color.gray);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            CheckFizzBuzz(currentRandomNumber);
+            SetTemporaryColor(fizzbuzz, Color.grey);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GenerateRandomNumber();
+            ClearTexts();
+        }
+    }
+
+    private void SetTemporaryColor(Image image, Color color)
+    {
+        image.color = color;
+        Invoke("ResetColor", 2f);
+    }
+
+    private void ResetColor()
+    {
+        fizz.color = Color.white;
+        buzz.color = Color.white;
+        fizzbuzz.color = Color.white;
+    }
+
+    private void CheckFizz(int number)
+    {
+        if (number % 3 == 0 && number % 5 != 0)
+        {
+            ShowResult(true);
+            PlaySound(correct);
+            Debug.Log("Fizz - Richtig!");
+            GenerateRandomNumber();
+            correctCount++;
+            UpdateCounterText();
+            hilfsText.text = "Richtig! Die Zahl ist durch 3 teilbar.";
+            mainCamera.backgroundColor = new Color(0.5f, 1.5f, 0.5f);
+        }
+        else
+        {
+            ShowResult(false);
+            PlaySound(wrong);
+            Debug.Log("Fizz - Falsch!");
+            hilfsText.text = "Fizz bedeutet, dass die Zahl durch 3 teilbar ist.";
+            erklaerungsText.text = "Dies ist sie, wenn ihre Quersumme durch 3 teilbar ist.";
+            mainCamera.backgroundColor = new Color(1f, 0.6f, 0.5f);
+        }
+    }
+
+    private void CheckBuzz(int number)
+    {
+        if (number % 5 == 0 && number % 3 != 0)
+        {
+            ShowResult(true);
+            PlaySound(correct);
+            Debug.Log("Buzz - Richtig!");
+            GenerateRandomNumber();
+            correctCount++;
+            UpdateCounterText();
+            hilfsText.text = "Richtig! Die Zahl ist durch 5 teilbar.";
+            mainCamera.backgroundColor = new Color(0.5f, 1.5f, 0.5f);
+        }
+        else
+        {
+            ShowResult(false);
+            PlaySound(wrong);
+            Debug.Log("Buzz - Falsch!");
+            hilfsText.text = "Buzz bedeutet, dass die Zahl durch 5 teilbar ist.";
+            erklaerungsText.text = "Dies ist sie, wenn die letzte Ziffer 0 bzw 5 ist.";
+            mainCamera.backgroundColor = new Color(1f, 0.6f, 0.5f);
+        }
+    }
+
+    private void CheckFizzBuzz(int number)
+    {
+        if (number % 3 == 0 && number % 5 == 0)
+        {
+            ShowResult(true);
+            PlaySound(correct);
+            Debug.Log("FizzBuzz - Richtig!");
+            GenerateRandomNumber();
+            correctCount++;
+            UpdateCounterText();
+            hilfsText.text = "Richtig! Die Zahl ist durch 3 und 5 teilbar.";
+            mainCamera.backgroundColor = new Color(0.5f, 1.5f, 0.5f);
+        }
+        else
+        {
+            ShowResult(false);
+            PlaySound(wrong);
+            Debug.Log("FizzBuzz - Falsch!");
+            hilfsText.text = "FizzBuzz bedeutet, dass die Zahl durch 3 und 5 teilbar ist.";
+            erklaerungsText.text = "Dies ist sie, wenn die Quersumme durch 3 teilbar ist oder die letzte Ziffer 0 bzw 5 ist.";
+            mainCamera.backgroundColor = new Color(1f, 0.6f, 0.5f);
+        }
+    }
+
+    private void GameWon()
+    {
+        EnableGameElements(false);
+        winText.gameObject.SetActive(true);
+        startAgainButton.gameObject.SetActive(true);
+    }
+
+    private void EnableGameElements(bool enable)
+    {
+        randomNumberText.gameObject.SetActive(enable);
+        hilfsText.gameObject.SetActive(enable);
+        erklaerungsText.gameObject.SetActive(enable);
+        counterText.gameObject.SetActive(enable);
+        fizz.gameObject.SetActive(enable);
+        buzz.gameObject.SetActive(enable);
+        fizzbuzz.gameObject.SetActive(enable);
+        newNumberButton.gameObject.SetActive(enable);
+    }
+
+    private void ClearTexts()
+    {
+        hilfsText.text = "";
+        erklaerungsText.text = "";
+    }
+
+    public void StartAgain()
+    {
+        InitializeGame();
+    }
+
+    private void PlaySound(AudioClip clip)
     {
         AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position);
     }
 
-    void CheckFizz(int number)
+    private void ShowResult(bool isCorrect)
     {
-        if (number % 3 == 0 && number % 5 != 0)
+        if (isCorrect)
         {
-            PlaySound(correct);
-            Debug.Log("Fizz - Stimmt!");
-            GenerateRandomNumber();
-            correctCount++;
-            UpdateCounterText();
-            hilfsText.text = "Richtig die Zahl ist durch 3 teilbar!";
-            maincam.backgroundColor = new Color(0.5f, 1.5f, 0.5f);
-
+            correctImage.gameObject.SetActive(true);
+            Invoke("HideResultImages", 2f);
         }
         else
         {
-            PlaySound(wrong);
-            Debug.Log("Fizz - Falsch!");
-            hilfsText.text = "Fizz bedeutet, dass die Zahl nur durch 3 teilbar sein muss.";
-            erklaerungsText.text = "Eine Zahl ist durch 3 teilbar, wenn die Quersumme der Ziffern durch 3 teilbar ist.";
-            maincam.backgroundColor = new Color(1f, 0.6f, 0.5f);
+            wrongImage.gameObject.SetActive(true);
+            Invoke("HideResultImages", 2f);
         }
     }
 
-    void CheckBuzz(int number)
+    private void HideResultImages()
     {
-        if (number % 5 == 0 && number % 3 != 0)
-        {
-            PlaySound(correct);
-            Debug.Log("Buzz - Stimmt!");
-            GenerateRandomNumber();
-            correctCount++;
-            UpdateCounterText();
-            hilfsText.text = "Richtig, die Zahl ist durch 5 teilbar!";
-            maincam.backgroundColor = new Color(0.5f, 1.5f, 0.5f);
-        }
-        else
-        {
-            PlaySound(wrong);
-            Debug.Log("Buzz - Falsch!");
-            hilfsText.text = "Buzz bedeutet, dass die Zahl nur durch 5 teilbar sein muss.";
-            erklaerungsText.text = "Eine Zahl ist durch 5 teilbar, wenn ihre letzte Ziffer 0 oder 5 ist.";
-            maincam.backgroundColor = new Color(1f, 0.6f, 0.5f);
-            
-        }
+        correctImage.gameObject.SetActive(false);
+        wrongImage.gameObject.SetActive(false);
     }
-
-    void CheckFizzBuzz(int number)
-    {
-        if (number % 3 == 0 && number % 5 == 0)
-        {
-            PlaySound(correct);
-            Debug.Log("FizzBuzz - Stimmt!");
-            GenerateRandomNumber();
-            correctCount++;
-            UpdateCounterText();
-            hilfsText.text = "Richtig, die Zahl ist durch 3 und 5 teilbar!";
-            maincam.backgroundColor = new Color(0.5f, 1.5f, 0.5f);
-        }
-        else
-        {
-            PlaySound(wrong);
-            Debug.Log("FizzBuzz - Falsch!");
-            hilfsText.text = "FizzBuzz musss durch 5 und 3 teilbar sein.";
-            erklaerungsText.text = "Eine Zahl ist durch 3 und 5 teilbar, wenn ihre letzte Ziffer 0 oder 5 ist, sowohl die Quersumme durch 3 teilbar ist.";
-            maincam.backgroundColor = new Color(1f, 0.6f, 0.5f);
-        }
-    }
-
-    void ResetColor()
-    {
-        fizz.color = Color.white;
-        buzz.color = Color.white;
-        fizzbuzz.color = Color.white; 
-        
-    }
-
 }
+
